3620dabbaffd89b077b325c588911d0fb620659f
RDAxRoadkill <wouter97@planet.nl>
2019-12-02 21:42:21 +0100

    added routes

--- server/routes/hardware.route.js
+++ server/routes/hardware.route.js
@@ -1,8 +1,20 @@
 const express = require('express');
 var routes = express.Router();
 const hardwareController = require('../controllers/hardware.controller');
+const specsController = require('../controllers/specs.controller');
 
-routes.post('/create', hardwareController.Create);
+//Hardware routes
 routes.get('/', hardwareController.Index);
+routes.post('/create', hardwareController.Create);
+routes.get('/:id', hardwareController.Read);
+routes.put('/:id', hardwareController.Edit);
+routes.delete('/:id', hardwareController.Delete);
+
+//Spec routes
+routes.get('/', specsController.Index);
+routes.post('/create', specsController.Create);
+routes.get('/:id', specsController.Read);
+routes.put('/:id', specsController.Edit);
+routes.delete('/:id', specsController.Delete);
 
 module.exports = routes;
