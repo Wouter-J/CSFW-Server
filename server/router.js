ce110a313250e32465f3bd856338e49b0e96491a
RDAxRoadkill <wouter97@planet.nl>
2019-12-04 15:22:33 +0100

    Finished basic CRUD services

--- server/router.js
+++ server/router.js
@@ -6,13 +6,13 @@ const authMiddleware = require('./middleware/auth');
 const hardwareRoute = require('./routes/hardware.route');
 const specsRoute = require('./routes/specs.route');
 const clientsRoute = require('./routes/clients.route');
-//const servicesRoute = require('./routes/services.route');
+const servicesRoute = require('./routes/services.route');
 const usersRoute = require('./routes/users.route');
 
 router.use('/api/hardware', hardwareRoute);
 router.use('/api/specs', specsRoute);
 router.use('/api/clients', clientsRoute);
-//router.use('/api/services', servicesRoute);
+router.use('/api/services', servicesRoute);
 //router.use('/api/users', usersRoute);
 
 //Catching all other requests
